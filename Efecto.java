371f0f0f464e63c8d62dd4895cd05676bd006b56
Andrick Avila Villaseñor <avilaandrick8@gmail.com>
2025-11-19 18:48:34 -0600

    Agregue un constructor y metodo que despues se sobreescribira

--- Efecto.java
+++ Efecto.java
@@ -1,5 +1,13 @@
 public abstract class Efecto {
-    public abstract String getNombre();
+    protected String nombre;
+    protected int duracion_turnos;
+
+    Efecto(String nombre, int duracion_turnos){
+        this.nombre = nombre;
+        this.duracion_turnos = duracion_turnos;
+    }
+
+    //Metodo que se sobreescribira dependiendo que efecto querramos usar:
+    public abstract void aplicarEfecto(Coche coche);
     
-    public abstract void aplicar(Coche coche) throws InterruptedException;
 }
