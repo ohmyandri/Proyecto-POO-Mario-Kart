44143e793c3513e92e8ce6c2b879db5237d1821f
Andrick Avila Villaseñor <avilaandrick8@gmail.com>
2025-11-19 18:38:15 -0600

    pequeno cambio en syntax del codigo

--- Efecto.java
+++ Efecto.java
@@ -1,4 +1,5 @@
-
-public class Efecto {
-
+public abstract class Efecto {
+    public abstract String getNombre();
+    
+    public abstract void aplicar(Coche coche) throws InterruptedException;
 }
