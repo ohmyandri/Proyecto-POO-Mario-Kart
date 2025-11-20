c723999400190c767674d193e7f9728e513de087
Andrick Avila Villaseñor <avilaandrick8@gmail.com>
2025-11-19 18:53:39 -0600

    Nueva clase implementando efecto de reducir de 1-6 a 1-3

---
+++ ReducirTurnos.java
@@ -0,0 +1,12 @@
+public class ReducirTurnos extends Efecto{
+
+    ReducirTurnos(String nombre, int duracion_turnos) {
+        super(nombre, duracion_turnos);
+    }
+
+    @Override
+    public void aplicarEfecto(Coche coche) {
+        throw new UnsupportedOperationException("Unimplemented method 'aplicarEfecto'");
+    }
+    
+}
