d02c2e681a9bc38024c9fc4ee4d60356ecaa0d0b
Andrick Avila Villaseñor <avilaandrick8@gmail.com>
2025-11-25 18:39:35 -0600

    Main documentado, con ciertos comentarios y nuevos participantes anadidos

--- Main.java
+++ Main.java
@@ -1,17 +1,30 @@
 public class Main {
     public static void main(String[] args) {
+        //Creando instancia de la carrera
         Carrera carrera = new Carrera();
 
-        Participante p1 = new Participante("Coche 1", carrera);
-        Participante p2 = new Participante("Coche 2", carrera);
-        Participante p3 = new Participante("Coche 3", carrera);
-        Participante p4 = new Participante("Coche 4", carrera);
+        //Creando las instancias de cada participante
+        Participante p1 = new Participante("Mario", carrera);
+        Participante p2 = new Participante("Luigi", carrera);
+        Participante p3 = new Participante("Peach", carrera);
+        Participante p4 = new Participante("Boo", carrera);
+        // Otras instancias que se podrian crear:
+        Participante p5 = new Participante("Toad", carrera);
+        Participante p6 = new Participante("Yoshi", carrera);
+        Participante p7 = new Participante("Daisy", carrera);
+        Participante p8 = new Participante("Wario", carrera);
 
+        //Agregamos los participantes a la lista de participantes de la carrera
         carrera.agregarParticipante(p1);
         carrera.agregarParticipante(p2);
         carrera.agregarParticipante(p3);
         carrera.agregarParticipante(p4);
+        carrera.agregarParticipante(p5);
+        carrera.agregarParticipante(p6);
+        carrera.agregarParticipante(p7);
+        carrera.agregarParticipante(p8);
 
+        //Usamos el metodo iniciar carrera el cual incluye para usar .start de los hilos
         carrera.iniciarCarrera();
     }
 }
